4e1d62657351949ff0f4c4d3fbcc0083ba1e7615
Richard Hua <Rich@Richard-Huas-MacBook-Pro.local>
2011-04-04 18:37:47 +1000

    Added a new file (richard.m)

---
+++ richard.h
@@ -0,0 +1,16 @@
+//
+//  richard.h
+//  Class3
+//
+//  Created by Richard Hua on 4/04/11.
+//  Copyright 2011 UNSW. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface richard : NSObject {
+
+}
+
+@end
---
+++ richard.m
@@ -0,0 +1,14 @@
+//
+//  richard.m
+//  Class3
+//
+//  Created by Richard Hua on 4/04/11.
+//  Copyright 2011 UNSW. All rights reserved.
+//
+
+#import "richard.h"
+
+
+@implementation richard
+
+@end
